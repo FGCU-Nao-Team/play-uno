d82bae1c71280518413f213e434a167d5f2c51dd
florap31 <fpierre1202@eagle.fgcu.edu>
2019-11-14 18:37:40 -0500

    Vision tkinter updates

--- UI.py
+++ UI.py
@@ -1,17 +1,24 @@
 import Tkinter as tk
 import threading
 from Tkinter import *
-
+import numpy as np
 import vision_definitions
-from PIL import Image
+from PIL import Image, ImageTk
 
-win = tk.Tk()
-win.geometry("1200x700+30+30")
-win.title("NAO plays UNO")
+
 
 
 class UIThread(threading.Thread):
-    def run(self, bot):
+
+    def run(self, bot, win):
+
+        textLabel = Label(win, text="Nao Vision")
+        textLabel.pack(side=TOP, pady=10)
+        winVision = Frame(win)
+        winVision.pack(side=TOP)
+        # Label in frame
+        vLabel = Label(winVision)
+        vLabel.pack()
         print 'UI Thread started'
         resolution = vision_definitions.kQVGA
         colorSpace = vision_definitions.kRGBColorSpace
@@ -20,8 +27,27 @@ class UIThread(threading.Thread):
         nameID = bot.camProxy.subscribe("python_client", resolution, colorSpace, fps)
         bot.camProxy.setResolution(nameID, resolution)
 
+        print 'Loop'
         while True:
             image = bot.camProxy.getImageRemote(nameID)
             im = Image.frombytes('RGB', (image[0], image[1]), image[6])
-            im.save("camImage.png", "PNG")
-            im.show()
+
+            print im
+            visual = np.asarray(im)
+            # Convert image format to tkinter compatible image
+            frame_image = ImageTk.PhotoImage(image=im)
+            # Display image to frame
+            vLabel.frame_image = frame_image
+            vLabel.config(image=frame_image)
+            vLabel.image = frame_image
+            win.mainloop()
+
+
+
+
+
+
+
+
+
+
--- Uno.py
+++ Uno.py
@@ -1,8 +1,12 @@
 from NAO import NAO
 from UI import UIThread
+from Tkinter import *
+import Tkinter as tk
+win = tk.Tk()
+win.geometry("1200x700+30+30")
+win.title("NAO plays UNO")
 
-
-naoRobot = NAO("10.100.164.55", 9559)
+naoRobot = NAO("10.100.140.239", 9559)
 
 guiThread = UIThread()
-guiThread.run(naoRobot)
+guiThread.run(naoRobot, win)
